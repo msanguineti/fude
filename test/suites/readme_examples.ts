624f5a848d90e64a84e3d1135a56a9872f448539
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2020-11-22 16:14:35 +0200

    feat: :sparkles: ANSI Codes

--- test/suites/readme_examples.ts
+++ test/suites/readme_examples.ts
@@ -3,30 +3,54 @@ import * as fude from '../../src'
 export const readmeExamples = (): void =>
   describe('readme', () => {
     test('usage examples', () => {
-      const output1 =
+      const ex1 =
         fude.fude('筆', fude.bgRed, fude.white) +
         fude.fude(' fude ', fude.bgWhite, fude.black)
 
       // alternatively...
-      const output2 =
-        fude.bgRed(fude.white`筆`) + fude.bgWhite(fude.black` fude `)
+      const ex2 = fude.bgRed(fude.white`筆`) + fude.bgWhite(fude.black` fude `)
 
       // template literals
-      const output3 =
+      const ex3 =
         fude.bgRed`${fude.white`筆`}` + fude.bgWhite`${fude.black` fude `}`
 
-      expect(output1).toMatchInlineSnapshot(`"[41m[37m筆[39m[49m[47m[30m fude [39m[49m"`)
-      expect(output1).toMatch(output2)
-      expect(output3).toMatch(output1)
+      expect(ex1).toMatchInlineSnapshot(`"[41m[37m筆[39m[49m[47m[30m fude [39m[49m"`)
+      expect(ex1).toMatch(ex2)
+      expect(ex3).toMatch(ex1)
 
       // or nested (same result, slightly different intentions with the backgrounds)...
-      const output4 = fude.bgRed(
+      const ex4 = fude.bgRed(
         fude.white('筆') + fude.bgWhite(` ${fude.black('fude')} `)
       )
 
       // template literals (also nested)...
-      const output5 = fude.bgRed`${fude.white`筆`}${fude.bgWhite` ${fude.black`fude`} `}`
+      const ex5 = fude.bgRed`${fude.white`筆`}${fude.bgWhite` ${fude.black`fude`} `}`
 
-      expect(output4).toMatch(output5)
+      expect(ex4).toMatch(ex5)
+    })
+
+    test('api examples', () => {
+      const ex1 = fude.fude(
+        'red text on white background',
+        fude.bgWhite,
+        fude.red
+      )
+
+      const ex2 = fude.bgWhite(fude.red('red text on white background'))
+
+      expect(ex1).toMatch(ex2)
+      expect(ex1).toMatchInlineSnapshot(`"[47m[31mred text on white background[39m[49m"`)
+
+      const ex3 = `${fude.red`red text`} and ${fude.bgWhite`${fude.blue`blue text on white background`}`}`
+
+      const ex4 =
+        `${fude.red`red text`}` +
+        ' and ' +
+        `${fude.bgWhite(fude.blue`blue text on white background`)}`
+
+      expect(ex3).toMatch(ex4)
+      expect(ex3).toMatchInlineSnapshot(
+        `"[31mred text[39m and [47m[34mblue text on white background[39m[49m"`
+      )
     })
   })
