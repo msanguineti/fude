bffad66b8b71cf5460ad055ece3bd4ffdfac8abb
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2020-11-27 19:32:40 +0200

    feat: :sparkles: ansi rgb & hex functions

--- src/codes.ts
+++ src/codes.ts
@@ -1,159 +1,159 @@
 /**
- * 文字装飾コード番号 - The character OrnamentCode to apply
+ * 文字装飾コード番号 - The character OrnamentCode (SGR Parameter) to apply
  *
- * @see https://en.wikipedia.org/wiki/ANSI_escape_code
+ * @see https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_parameters
  */
 
 export type OrnamentCode = number
 
-/** @constant reset 	All attributes off*/
+/** @constant reset. All attributes off.*/
 export const resetCode: OrnamentCode = 0
 
-/** @constant bold  	As with faint/dim, the color change is a PC (SCO / CGA) invention.*/
+/** @constant bold. As with faint/dim, the color change is a PC (SCO / CGA) invention.*/
 export const boldCode: OrnamentCode = 1
 
 /** @constant dim  (with a saturated color). May be implemented as a light font weight like bold.*/
 export const dimCode: OrnamentCode = 2
 
-/** @constant italic 	Not widely supported. Sometimes treated as inverse or blink.*/
+/** @constant italic. Not widely supported.. Sometimes treated as inverse or blink.*/
 export const italicCode: OrnamentCode = 3
 
-/** @constant underline 	Style extensions exist for Kitty, VTE, mintty and iTerm2.*/
+/** @constant underline.. Style extensions exist for Kitty, VTE, mintty and iTerm2.*/
 export const underlineCode: OrnamentCode = 4
 
-/** @constant blinkSlow 	less than 150 per minute*/
+/** @constant blinkSlow; less than 150 per minute.*/
 export const blinkSlowCode: OrnamentCode = 5
 
-/** @constant blinkRapid 	MS-DOS ANSI.SYS, 150+ per minute; not widely supported*/
+/** @constant blinkRapid. MS-DOS ANSI.SYS, 150+ per minute; not widely supported.*/
 export const blinkRapidCode: OrnamentCode = 6
 
-/** @constant inverse 	swap foreground and background colors, aka invert; inconsistent emulation*/
-export const inverseCode: OrnamentCode = 7
+/** @constant invert; swap foreground and background colors; inconsistent emulation.*/
+export const invertCode: OrnamentCode = 7
 
-/** @constant hide  not widely supported.*/
+/** @constant hide; not widely supported.*/
 export const hideCode: OrnamentCode = 8
 
-/** @constant strikethrough  characters legible but marked as if for deletion.*/
+/** @constant strikethrough. Characters legible but marked as if for deletion.*/
 export const strikethroughCode: OrnamentCode = 9
 
-/** @constant defaultFont  Primary (default) font*/
+/** @constant defaultFont. Primary (default) font.*/
 export const defaultFontCode: OrnamentCode = 10
 
-/** @constant altFont1Code: Alternative font */
+/** @constant altFont1Code: alternative font */
 export const altFont1Code: OrnamentCode = 11
 
-/** @constant altFont2Code: Alternative font */
+/** @constant altFont2Code: alternative font */
 export const altFont2Code: OrnamentCode = 12
 
-/** @constant altFont3Code: Alternative font */
+/** @constant altFont3Code: alternative font */
 export const altFont3Code: OrnamentCode = 13
 
-/** @constant altFont4Code: Alternative font */
+/** @constant altFont4Code: alternative font */
 export const altFont4Code: OrnamentCode = 14
 
-/** @constant altFont5Code: Alternative font */
+/** @constant altFont5Code: alternative font */
 export const altFont5Code: OrnamentCode = 15
 
-/** @constant altFont6Code: Alternative font */
+/** @constant altFont6Code: alternative font */
 export const altFont6Code: OrnamentCode = 16
 
-/** @constant altFont7Code: Alternative font */
+/** @constant altFont7Code: alternative font */
 export const altFont7Code: OrnamentCode = 17
 
-/** @constant altFont8Code: Alternative font */
+/** @constant altFont8Code: alternative font */
 export const altFont8Code: OrnamentCode = 18
 
-/** @constant altFont9Code: Alternative font */
+/** @constant altFont9Code: alternative font */
 export const altFont9Code: OrnamentCode = 19
 
-/** @constant fraktur  Rarely supported*/
+/** @constant fraktur. Rarely supported.*/
 export const frakturCode: OrnamentCode = 20
 
-/** @constant doublyUnderline  Doubly underline or Bold off 	Double-underline per ECMA-48.:8.3.117 See discussion*/
+/** @constant doublyUnderline. Doubly underline or Bold off. Double-underline per ECMA-48.:8.3.117. See discussion.*/
 export const doublyUnderlineCode: OrnamentCode = 21
 
-/** @constant normalColor  Normal color or intensity 	Neither bold nor faint/dim*/
+/** @constant normalColor. Normal color or intensity. Neither bold nor faint/ dim.*/
 export const normalColorCode: OrnamentCode = 22
 
-/** @constant resetItalic Not italic, not Fraktur*/
+/** @constant resetItalic Not italic, not Fraktur.*/
 export const resetItalicCode: OrnamentCode = 23
 
-/** @constant resetUnderline  Underline off 	Not singly or doubly underlined*/
+/** @constant resetUnderline. Underline off. Not singly or doubly underlined.*/
 export const resetUnderlineCode: OrnamentCode = 24
 
-/** @constant resetBlink  Blink off*/
+/** @constant resetBlink. Blink off.*/
 export const resetBlinkCode: OrnamentCode = 25
 
-/** @constant propSpacing Proportional spacing 	ITU T.61 and T.416, not known to be used on terminals*/
+/** @constant propSpacing Proportional spacing. ITU T.61 and T.416, not known to be used on terminals.*/
 export const propSpacingCode: OrnamentCode = 26
 
-/** @constant resetInverse Reverse/invert off*/
+/** @constant resetInverse Reverse/invert off.*/
 export const resetInverseCode: OrnamentCode = 27
 
-/** @constant resetHide Reveal 	conceal/hide off*/
+/** @constant resetHide Reveal; conceal/hide off.*/
 export const resetHideCode: OrnamentCode = 28
 
-/** @constant resetStrikethrough Not crossed out*/
+/** @constant resetStrikethrough Not crossed out.*/
 export const resetStrikethroughCode: OrnamentCode = 29
 
-/** @constant black Set foreground color*/
+/** @constant black. Set foreground color.*/
 export const blackCode: OrnamentCode = 30
 
-/** @constant red Set foreground color*/
+/** @constant red. Set foreground color.*/
 export const redCode: OrnamentCode = 31
 
-/** @constant green Set foreground color*/
+/** @constant green. Set foreground color.*/
 export const greenCode: OrnamentCode = 32
 
-/** @constant yellow Set foreground color*/
+/** @constant yellow. Set foreground color.*/
 export const yellowCode: OrnamentCode = 33
 
-/** @constant blue Set foreground color*/
+/** @constant blue. Set foreground color.*/
 export const blueCode: OrnamentCode = 34
 
-/** @constant magenta Set foreground color*/
+/** @constant magenta. Set foreground color.*/
 export const magentaCode: OrnamentCode = 35
 
-/** @constant cyan Set foreground color*/
+/** @constant cyan. Set foreground color.*/
 export const cyanCode: OrnamentCode = 36
 
-/** @constant white Set foreground color*/
+/** @constant white. Set foreground color.*/
 export const whiteCode: OrnamentCode = 37
 
-/** @constant resetForeground Default foreground color 	implementation defined (according to standard)*/
+/** @constant resetForeground Default foreground color; implementation defined (according to standard).*/
 export const resetForegroundCode: OrnamentCode = 39
 
-/** @constant bgBlack Set background color*/
+/** @constant bgBlack. Set background color.*/
 export const bgBlackCode: OrnamentCode = 40
 
-/** @constant bgRed Set background color*/
+/** @constant bgRed. Set background color.*/
 export const bgRedCode: OrnamentCode = 41
 
-/** @constant bgGreen Set background color*/
+/** @constant bgGreen. Set background color.*/
 export const bgGreenCode: OrnamentCode = 42
 
-/** @constant bgYellow Set background color*/
+/** @constant bgYellow. Set background color.*/
 export const bgYellowCode: OrnamentCode = 43
 
-/** @constant bgBlue Set background color*/
+/** @constant bgBlue. Set background color.*/
 export const bgBlueCode: OrnamentCode = 44
 
-/** @constant bgMagenta Set background color*/
+/** @constant bgMagenta. Set background color.*/
 export const bgMagentaCode: OrnamentCode = 45
 
-/** @constant bgCyan Set background color*/
+/** @constant bgCyan. Set background color.*/
 export const bgCyanCode: OrnamentCode = 46
 
-/** @constant bgWhite Set background color*/
+/** @constant bgWhite. Set background color.*/
 export const bgWhiteCode: OrnamentCode = 47
 
-/** @constant bgReset Default background color 	implementation defined (according to standard)*/
+/** @constant bgReset Default background color; implementation defined (according to standard).*/
 export const resetBackgroundCode: OrnamentCode = 49
 
-/** @constant resetPropSpacing Disable proportional spacing 	T.61 and T.416*/
+/** @constant resetPropSpacing Disable proportional spacing. T.61 and T.416*/
 export const resetPropSpacingCode: OrnamentCode = 50
 
-/** @constant framed 	Implemented as "emoji variation selector" in mintty.*/
+/** @constant framed. Implemented as "emoji variation selector" in mintty.*/
 export const framedCode: OrnamentCode = 51
 
 /** @constant encircled Implemented as "emoji variation selector" in mintty.*/
@@ -162,91 +162,83 @@ export const encircledCode: OrnamentCode = 52
 /** @constant overlined Implemented as "emoji variation selector" in mintty.*/
 export const overlinedCode: OrnamentCode = 53
 
-/** @constant resetFramed  Not framed or encircled*/
+/** @constant resetFramed. Not framed or encircled.*/
 export const resetFramedCode: OrnamentCode = 54
 
-/** @constant resetOverlines Not overlined*/
+/** @constant resetOverlines Not overlined.*/
 export const resetOverlinesCode: OrnamentCode = 55
 
-/** @constant ideogramU ideogram underline or right side line 	Rarely supported*/
+/** @constant ideogramU ideogram underline or right side line. Rarely supported.*/
 export const ideogramUCode: OrnamentCode = 60
 
-/** @constant ideogramDU ideogram double underline or  double line on the right side*/
+/** @constant ideogramDU ideogram double underline on; double line on the right side.*/
 export const ideogramDUCode: OrnamentCode = 61
 
-/** @constant ideogramO ideogram overline or left side line*/
+/** @constant ideogramO ideogram overline or left side line.*/
 export const ideogramOCode: OrnamentCode = 62
 
-/** @constant ideogramDO ideogram double overline or  double line on the left side*/
+/** @constant ideogramDO ideogram double overline on; double line on the left side.*/
 export const ideogramDOCode: OrnamentCode = 63
 
-/** @constant ideogramS ideogram stress marking*/
+/** @constant ideogramS ideogram stress marking.*/
 export const ideogramSCode: OrnamentCode = 64
 
-/** @constant ideogramOff ideogram attributes off 	reset the effects of all of 60–64*/
+/** @constant ideogramOff ideogram attributes off; reset the effects of all of 60–64*/
 export const ideogramOffCode: OrnamentCode = 65
 
-/** @constant superscript mintty (not in standard)*/
+/** @constant superscript mintty (not in standard).*/
 export const superscriptCode: OrnamentCode = 73
 
-/** @constant subscript mintty (not in standard)*/
+/** @constant subscript mintty (not in standard).*/
 export const subscriptCode: OrnamentCode = 74
 
-/** @constant gray  Set bright foreground color 	aixterm (not in standard)*/
+/** @constant gray.. Set bright foreground color; aixterm (not in standard).*/
 export const grayCode: OrnamentCode = 90
 
-/** @constant brightRed Set bright foreground color 	aixterm (not in standard)*/
+/** @constant brightRed. Set bright foreground color; aixterm (not in standard).*/
 export const brightRedCode: OrnamentCode = 91
 
-/** @constant brightGreen Set bright foreground color 	aixterm (not in standard)*/
+/** @constant brightGreen. Set bright foreground color; aixterm (not in standard).*/
 export const brightGreenCode: OrnamentCode = 92
 
-/** @constant brightYellow Set bright foreground color 	aixterm (not in standard)*/
+/** @constant brightYellow. Set bright foreground color; aixterm (not in standard).*/
 export const brightYellowCode: OrnamentCode = 93
 
-/** @constant brightBlue Set bright foreground color 	aixterm (not in standard)*/
+/** @constant brightBlue. Set bright foreground color; aixterm (not in standard).*/
 export const brightBlueCode: OrnamentCode = 94
 
-/** @constant brightMagenta Set bright foreground color 	aixterm (not in standard)*/
+/** @constant brightMagenta. Set bright foreground color; aixterm (not in standard).*/
 export const brightMagentaCode: OrnamentCode = 95
 
-/** @constant brightCyan Set bright foreground color 	aixterm (not in standard)*/
+/** @constant brightCyan. Set bright foreground color; aixterm (not in standard).*/
 export const brightCyanCode: OrnamentCode = 96
 
-/** @constant brightWhite Set bright foreground color 	aixterm (not in standard)*/
+/** @constant brightWhite. Set bright foreground color; aixterm (not in standard).*/
 export const brightWhiteCode: OrnamentCode = 97
 
-/** @constant bgBrightBlack Set bright background color*/
+/** @constant bgBrightBlack. Set bright background color.*/
 export const bgBrightBlackCode: OrnamentCode = 100
 
-/** @constant bgBrightRed Set bright background color*/
+/** @constant bgBrightRed. Set bright background color.*/
 export const bgBrightRedCode: OrnamentCode = 101
 
-/** @constant bgBrightGreen Set bright background color*/
+/** @constant bgBrightGreen. Set bright background color.*/
 export const bgBrightGreenCode: OrnamentCode = 102
 
-/** @constant bgBrightYellow Set bright background color*/
+/** @constant bgBrightYellow. Set bright background color.*/
 export const bgBrightYellowCode: OrnamentCode = 103
 
-/** @constant bgBrightBlue Set bright background color*/
+/** @constant bgBrightBlue. Set bright background color.*/
 export const bgBrightBlueCode: OrnamentCode = 104
 
-/** @constant bgBrightMagenta Set bright background color*/
+/** @constant bgBrightMagenta. Set bright background color.*/
 export const bgBrightMagentaCode: OrnamentCode = 105
 
-/** @constant bgBrightCyan Set bright background color*/
+/** @constant bgBrightCyan. Set bright background color.*/
 export const bgBrightCyanCode: OrnamentCode = 106
 
-/** @constant bgBrightWhite Set bright background color*/
+/** @constant bgBrightWhite. Set bright background color.*/
 export const bgBrightWhiteCode: OrnamentCode = 107
-
-/**
- *
- * @param text the input string
- * @param codes the OrnamentCode codes to apply to the input
- */
-export const ansi = (text: string, ...codes: OrnamentCode[]): string =>
-  `\x1b[${codes.join(';')}m${text}\x1b[0m`
 
 /**
  * Prints what is available on your TTY
--- src/core.ts
+++ src/core.ts
@@ -1,14 +1,61 @@
+import { OrnamentCode } from './codes'
 import { CharacterOrnament } from './ornaments'
 
 /**
- * 書く - composes the various text ornaments to obtain the final output string
+ * This code was taken from Kleur (https://github.com/lukeed/kleur) and adapted for our use case.
  *
- * @param tos set of text ornaments
- * @param text the input string
- * @returns a string with the applied text ornaments
+ * FORCE_COLOR should decide whether to force colors or not.
  */
-const kaku = (tos: CharacterOrnament[]) => (text: string) =>
-  tos.reduceRight((v, f) => f(v), text)
+const { FORCE_COLOR, NODE_DISABLE_COLORS, NO_COLOR, TERM } = process.env
+const isTTY = process.stdout && process.stdout.isTTY
+let enabled = FORCE_COLOR
+  ? !/^\s*(?:false|0|off)\s*$/i.test(FORCE_COLOR)
+  : !NODE_DISABLE_COLORS && NO_COLOR == null && TERM !== 'dumb' && isTTY
+
+export const setEnabled = (colorsEnabled: boolean): void => {
+  enabled = colorsEnabled
+}
+
+export const isEnabled = (): boolean => enabled
+
+const templates = (txt, keys, opt) => {
+  let ret = txt[0]
+  for (let i = 0; i < keys.length; i++) {
+    ret += keys[i] + txt[i + 1]
+  }
+
+  return enabled
+    ? opt.open +
+        (ret.indexOf(opt.close) >= 0 ? ret.replace(opt.rgx, opt.open) : ret) +
+        opt.close
+    : ret
+}
+/**
+ * 筆致 - Only apply the given attribute to the input string if we are in a TTY environment or if the FORCE_COLOR flag is set.
+ *
+ * @param text the input string
+ * @param on the attribute on code
+ * @param off the attribute off code
+ * @returns a string with the applied attribute
+ */
+export const hitchi = (
+  on: OrnamentCode,
+  off: OrnamentCode = 0
+): ((...text: string[]) => string) => {
+  const opt = {
+    open: `\x1b[${on}m`,
+    close: `\x1b[${off}m`,
+    rgx: new RegExp(`\\x1b\\[(?:${off})m`, 'gi'),
+  }
+  return (txt, ...keys): string =>
+    keys.length !== 0
+      ? templates(txt, keys, opt)
+      : enabled
+      ? opt.open +
+        (txt.indexOf(opt.close) >= 0 ? txt.replace(opt.rgx, opt.open) : txt) +
+        opt.close
+      : txt + ''
+}
 
 /**
  * 筆 - Takes a string and applies the specified text ornaments to it.
@@ -16,5 +63,256 @@ const kaku = (tos: CharacterOrnament[]) => (text: string) =>
  * @param text the input string
  * @param tos text ornaments to apply
  */
-export const fude = (text: string, ...tos: CharacterOrnament[]): string =>
-  kaku(tos)(text)
+export const fude = (
+  text: string | CharacterOrnament,
+  ...tos: CharacterOrnament[]
+): string => tos.reduceRight((v, f) => f(v), text as string)
+
+// match close attributes escape codes at the end of a given string
+const rgx = new RegExp(`(?:\\x1b\\[[0-9]{1,3}m)+$`, 'i')
+
+const closeAttribute = {
+  0: 0,
+  1: 22,
+  2: 22,
+  3: 23,
+  4: 24,
+  21: 24,
+  5: 25,
+  6: 26,
+  7: 27,
+  8: 28,
+  9: 29,
+  11: 10,
+  12: 10,
+  13: 10,
+  14: 10,
+  15: 10,
+  16: 10,
+  17: 10,
+  18: 10,
+  19: 10,
+  20: 23,
+  26: 50,
+  30: 39,
+  31: 39,
+  32: 39,
+  33: 39,
+  34: 39,
+  35: 39,
+  36: 39,
+  37: 39,
+  38: 39,
+  40: 49,
+  41: 49,
+  42: 49,
+  43: 49,
+  44: 49,
+  45: 49,
+  46: 49,
+  47: 49,
+  48: 49,
+  51: 54,
+  52: 54,
+  53: 55,
+  58: 59,
+  60: 65,
+  61: 65,
+  62: 65,
+  63: 65,
+  64: 65,
+  73: 10,
+  74: 10,
+  90: 39,
+  91: 39,
+  92: 39,
+  93: 39,
+  94: 39,
+  95: 39,
+  96: 39,
+  97: 39,
+  100: 49,
+  101: 49,
+  102: 49,
+  103: 49,
+  104: 49,
+  105: 49,
+  106: 49,
+  107: 49,
+}
+
+/**
+ * Pass ANSI SGR attributes codes directly
+ *
+ * @param text the input string
+ * @param codes the OrnamentCode codes to apply to the input
+ */
+export const ansi = (
+  text: string | CharacterOrnament,
+  ...codes: OrnamentCode[]
+): string =>
+  enabled
+    ? `\x1b[${codes.join(';')}m` +
+      ((text as string).indexOf('\x1b[') >= 0
+        ? (text as string).replace(rgx, '')
+        : text) +
+      `\x1b[${codes.map((c) => closeAttribute[c] ?? 0)}m`
+    : (text as string)
+
+const applyRGB = (
+  r: number,
+  g: number,
+  b: number,
+  text: string | CharacterOrnament,
+  open,
+  close
+): string =>
+  hitchi(
+    (`${open};${2};${r};${g};${b}` as unknown) as number,
+    close
+  )(text as string)
+
+const openRGBColorsAttribute = 38
+const closeRGBColorsAttribute = 39
+
+/**
+ * Apply an RGB foreground color to the given input text.
+ *
+ * @param text the input string
+ * @param r red color value (0-255) default 0
+ * @param g green color value (0-255) default 0
+ * @param b blue color attribute (0-255) default 0
+ */
+export const rgb = (
+  text: string | CharacterOrnament,
+  { r = 0, g = 0, b = 0 }: { r?: number; g?: number; b?: number }
+): string =>
+  enabled
+    ? applyRGB(r, g, b, text, openRGBColorsAttribute, closeRGBColorsAttribute)
+    : (text as string)
+
+const openRGBBGColorsAttribute = 48
+const closeRGBBGColorsAttribute = 49
+
+/**
+ * Apply an RGB background color to the given input text.
+ *
+ * @param text the input string
+ * @param r red color value (0-255) default 0
+ * @param g green color value (0-255) default 0
+ * @param b blue color attribute (0-255) default 0
+ */
+export const rgbBg = (
+  text: string | CharacterOrnament,
+  { r = 0, g = 0, b = 0 }: { r?: number; g?: number; b?: number }
+): string =>
+  enabled
+    ? applyRGB(
+        r,
+        g,
+        b,
+        text,
+        openRGBBGColorsAttribute,
+        closeRGBBGColorsAttribute
+      )
+    : (text as string)
+
+const openRGBUnderlineColorsAttribute = 58
+const closeRGBUnderlineColorsAttribute = 59
+
+/**
+ * Apply an RGB color to the underline of the given input text.
+ *
+ * @param text the input string
+ * @param r red color value (0-255) default 0
+ * @param g green color value (0-255) default 0
+ * @param b blue color attribute (0-255) default 0
+ * @param double doubly underline default to `false`
+ */
+export const rgbUnderline = (
+  text: string | CharacterOrnament,
+  {
+    r = 0,
+    g = 0,
+    b = 0,
+    double = false,
+  }: { r?: number; g?: number; b?: number; double?: boolean }
+): string =>
+  enabled
+    ? applyRGB(
+        r,
+        g,
+        b,
+        double ? hitchi(21, 24)(text as string) : hitchi(4, 24)(text as string),
+        openRGBUnderlineColorsAttribute,
+        closeRGBUnderlineColorsAttribute
+      )
+    : (text as string)
+
+/**
+ * Apply a HEX foreground color value to the given input text.
+ *
+ * Case insensitive, with or without `#` as a prefix.
+ *
+ * @param text the input string
+ * @param hex the color value as an hex number
+ */
+export const hex = (text: string | CharacterOrnament, hex: string): string => {
+  const match = /([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})/i.exec(hex)
+
+  return match
+    ? rgb(text, {
+        r: parseInt(match[1], 16),
+        g: parseInt(match[2], 16),
+        b: parseInt(match[3], 16),
+      })
+    : (text as string)
+}
+
+/**
+ * Apply a HEX background color value to the given input text.
+ *
+ * Case insensitive, with or without `#` as a prefix.
+ *
+ * @param text the input string
+ * @param hex the color value as an hex number
+ */
+export const hexBg = (
+  text: string | CharacterOrnament,
+  hex: string
+): string => {
+  const match = /([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})/i.exec(hex)
+
+  return match
+    ? rgbBg(text, {
+        r: parseInt(match[1], 16),
+        g: parseInt(match[2], 16),
+        b: parseInt(match[3], 16),
+      })
+    : (text as string)
+}
+
+/**
+ * Apply a HEX color value to the underline of the given input text.
+ *
+ * Case insensitive, with or without `#` as a prefix.
+ *
+ * @param text the input string
+ * @param hex the color value as an hex number
+ */
+export const hexUnderline = (
+  text: string | CharacterOrnament,
+  hex: string,
+  double = false
+): string => {
+  const match = /([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})/i.exec(hex)
+
+  return match
+    ? rgbUnderline(text, {
+        r: parseInt(match[1], 16),
+        g: parseInt(match[2], 16),
+        b: parseInt(match[3], 16),
+        double,
+      })
+    : (text as string)
+}
